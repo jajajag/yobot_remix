0ba6e841b91f9231bed8d01c36fcce72cbeee9fe
Jag Zhang <jajajag@users.noreply.github.com>
2025-06-03 00:43:48 -0400

    Update 出刀记录.

--- src/client/ybplugins/clan_battle/components/realize.py
+++ src/client/ybplugins/clan_battle/components/realize.py
@@ -1455,8 +1455,8 @@ def challenge_record(self, group_id):
 
 	total_blade_num = 0				#总出刀数
 	total_continue_blade_num = 0	#总补偿刀数量
-	zero_blade_members = []			#一刀没出的成员
-	blade_list = {}
+    count_blade_members = {}   #统计没出满刀的成员
+	#blade_list = {}
 	for member in members:
 		challenge_records:List[Clan_challenge] = Clan_challenge.select().where(
 			Clan_challenge.gid == group_id,
@@ -1468,7 +1468,8 @@ def challenge_record(self, group_id):
 			member_num = 0			#单个成员出刀数
 			continue_blade_num = 0	#单个成员剩余补偿刀数量
 			for c in challenge_records:
-				if c.boss_health_remain == 0 and not c.is_continue:	#完整刀收尾算0.5刀
+                #完整刀收尾算0.5刀
+				if c.boss_health_remain == 0 and not c.is_continue:
 					member_num += 0.5
 					continue_blade_num += 1
 				elif c.is_continue:	#补偿刀算0.5刀
@@ -1477,19 +1478,30 @@ def challenge_record(self, group_id):
 				else: member_num += 1
 			total_blade_num += member_num
 			total_continue_blade_num += continue_blade_num
-			if member_num not in blade_list: blade_list[member_num] = 1
-			else: blade_list[member_num] += 1
+            # JAG: Use count_blade_members instead
+			#if member_num not in blade_list: blade_list[member_num] = 1
+			#else: blade_list[member_num] += 1
+            # JAG: 统计每个成员的出刀数量
+            if member_num not in count_blade_members:
+                count_blade_members[member_num] = []
+            count_blade_members[member_num].append(member.qqid)
 		else:
-			zero_blade_members.append(member.qqid)
+			count_blade_members[0].append(member.qqid)
 
 	back_msg = []
 	back_msg.append(f"待出补偿刀数量：{total_continue_blade_num}")
-	back_msg.append(f"已出0刀的成员数量：{len(zero_blade_members)}")
-	for i in range(len(zero_blade_members)):
-		name = self._get_nickname_by_qqid(zero_blade_members[i])
-		back_msg.append(f"{i == len(zero_blade_members)-1 and '┖' or '┣'}{name}")
-	for blade_num in blade_list.keys():
-		back_msg.append(f"已出{blade_num}刀：{blade_list[blade_num]}")
+	#back_msg.append(f"已出0刀的成员数量：{len(zero_blade_members)}")
+    for blade_num in sorted(count_blade_members.keys()):
+	    back_msg.append(
+                f"已出{blade_num}刀：{len(count_blade_members[blade_num])}")
+        # JAG: 出完刀的成员不显示
+        if blade_num >= 3:
+            continue
+	    for i in range(len(count_blade_members[blade_num])):
+	    	name = self._get_nickname_by_qqid(count_blade_members[blade_num][i])
+	    	back_msg.append(f"{i == len(count_blade_members[blade_count])-1 and '┖' or '┣'}{name}")
+	#for blade_num in blade_list.keys():
+	#	back_msg.append(f"已出{blade_num}刀：{blade_list[blade_num]}")
 	back_msg.append(f"今天已出 {total_blade_num}/{len(members)*3}")
 	return '\n'.join(back_msg)
 
